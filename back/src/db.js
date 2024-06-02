5cef9aaed00467ec15f6ff788c14942ba97c0363
SantiBcra <santiariel1705@gmail.com>
2024-06-02 10:40:13 -0300

    dd

--- back/src/db.js
+++ back/src/db.js
@@ -2,19 +2,15 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize');
 const fs = require("fs");
 const path = require("path");
-const {DB_USER, DB_PASSWORD, DB_PORT,DB_HOST,DB_DIALECT,DB_NAME  } = process.env;
+const {DB_USER, DB_PASSWORD, DB_PORT,DB_HOST,DB_DIALECT,DB_URL,DB_NAME  } = process.env;
 
 console.log(DB_USER)
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+const sequelize = new Sequelize(DB_URL, {
   host: DB_HOST,
   dialect: DB_DIALECT,
-  ssl: true, // Habilita SSL
-  dialectOptions: {
-    ssl: {
-      require: true, // Requerir SSL
-      rejectUnauthorized: false // Opción para evitar errores con certificados autofirmados o no válidos
-    }
-  }
+ 
+  
+  
 });
 
 const basename = path.basename(__filename);
